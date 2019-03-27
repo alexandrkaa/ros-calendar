66cfa0370406fb5c1949a4cb49e816e57d29661c
Alexander Komarov <komar81@gmail.com>
2019-03-27 07:37:37 +0300

    axios added

--- js/app.js
+++ js/app.js
@@ -1,3 +1,12 @@
+getApiData = function (reqParams) {
+  // Send a POST request
+  return axios({
+    method: 'post',
+    url: 'api.json',
+    params: reqParams
+  });
+};
+
 $(document).ready(function () {
   $('.events__event-link, .popup-layer, .popup__close').click(function() {
     $('.popup-layer').toggleClass('popup-layer--visible');
@@ -9,24 +18,36 @@ $(document).ready(function () {
 
   $('.apply-form').on('submit', function(e) {
     e.preventDefault();
-    $.ajax({
-      url: '/api.json',
-      type: 'get',
-      dataType: 'json',
-      cache: false,
-      contentType: false,
-      processData: false,
-      data: {
-        'meetupName': $('.meetup_name').val() || "123",
-      },
-      beforeSend: function() {
-        $('.meetup_name').attr('disabled', 'disabled');
-      }
-    })
-    .done(function(msg) {
-      $('.meetup_name').removeAttr('disable');
-      // console.log(msg[0]);
-      $('.vote__list').prepend('<li class="vote__item">' + $('.meetup_name').val()+' <button class="vote__like-button"><span class="visually-hidden">like</span></button></li>');
-    });
+    // $.ajax({
+    //   url: 'api.json',
+    //   type: 'get',
+    //   dataType: 'json',
+    //   cache: false,
+    //   contentType: false,
+    //   processData: false,
+    //   data: {
+    //     'meetupName': $('.meetup_name').val() || "123",
+    //   },
+    //   beforeSend: function() {
+    //     $('.meetup_name').attr('disabled', 'disabled');
+    //   }
+    // })
+    // .done(function(msg) {
+    //   $('.meetup_name').removeAttr('disabled');
+    //   $('.vote__list').prepend('<li class="vote__item">' + $('.meetup_name').val()+' <button class="vote__like-button"><span class="visually-hidden">like</span></button></li>');
+    // });
+    var reqParams = {
+      'meetupName': $('.meetup_name').val() || "123"
+    }
+
+    getApiData(reqParams)
+      .then(function (response) {
+        // console.log(response);
+        $('.meetup_name').removeAttr('disabled');
+        $('.vote__list').prepend('<li class="vote__item">' + $('.meetup_name').val() + ' <button class="vote__like-button"><span class="visually-hidden">like</span></button></li>');
+      })
+      .catch(function (err) {
+        console.log(err);
+      });
   });
 });
